ac69fd3103475c9529beab7101af4ef6b35b783d
Alexandre Fco <112628084+lexxUnivali@users.noreply.github.com>
2023-05-07 23:30:25 -0300

    Arquivos principais do projeto

---
+++ MKR RENT A CAR - CRIAR BANCO E TABELAS - ALEXANDRE FRANCISCO DE SOUZA.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO mkr_rent_a_car.cliente (Id, c_nome, c_email, c_senha) VALUES 
+('1', 'Alexandre Francisco de Souza', 'alex.souza@edu.univali.br', '55555555'),
+('2', 'João Carlos Vieira', 'joao@edu.univali.br', '6666666'),
+('3', 'Claudete Baltazar', 'Claudete@edu.univali.br', '77777'),
+('4', 'Leandro Santa', 'lelesa@edu.univali.br', '77777'),
+('10', 'Felipe Camargo', 'felipao@edu.univali.br', '77777'),
+('6', 'Eduardo Silva', 'dudusilva@edu.univali.br', '77777'),
+('7', 'Maria de Jesus', 'mjesus@edu.univali.br', '77777'),
+('8', 'Cleiton machado', 'cleitonma@edu.univali.br', '77777'),
+('9', 'Alessandro tomate', 'aletomate@edu.univali.br', '77777'),
+('5', 'Molusco Desonesto', 'AmorVenceu2@edu.univali.br', '666');
